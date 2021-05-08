424d4b81c33d77396c77d2b85cfb15153565fcbc
Ayushmaan Mani Tripathi <ayushmaaan123@gmail.com>
2021-05-09 01:15:23 +0530

    User Input

--- src/MinMax.java
+++ src/MinMax.java
@@ -1,9 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class MinMax {
-    public static void main(String[] args) {
-        int a[] = {2,3,5,7,12,35,568,78,12,46,221};
-        int min=a[0],max=a[0];
-        for(int i=0;i<a.length; i++)
+    public static void main(String[] args)throws IOException {
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        int i =0;
+        System.out.println("Enter Length of the Array");
+        int l = Integer.parseInt(br.readLine());
+        int a[]= new int[l];
+        System.out.println("Enter the Numbers");
+        for (i=0;i<l;i++)
         {
+            a[i] = Integer.parseInt(br.readLine());
+        }
+        int min=a[0],max=a[0];
+        for(i=0;i<a.length; i++) {
             if(min>a[i])
             {
                 min = a[i];
